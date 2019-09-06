79d66ae96b20d05e5954883eec6efb95640ca8d8
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-06 17:38:52 +0100

    Update GPS.h

--- GPS.h
+++ GPS.h
@@ -5,17 +5,16 @@
 
 class GPS {
   private:
+    SoftwareSerial *_serial;
     uint16_t _length;
 
   public:
-    SoftwareSerial *_serial;
-
     explicit GPS(uint8_t rx, uint16_t length = 800): _length(length) {
       _serial = new SoftwareSerial(rx, rx);
     }
 
-    void begin(uint32_t baud) {
-      _serial->begin(baud);
+    void begin(uint32_t baud) {_serial->begin(baud);
+      _serial->begin(9600);
     }
 
     String readString() {
@@ -23,11 +22,16 @@ class GPS {
       uint16_t lenght = _length;
       while (!_serial->available() || (_serial->read() != 0x24));
       do if (_serial->available() && (data += char(_serial->read())) && lenght--); while (lenght);
-      data.remove(0, data.indexOf(char(0x0D)));
+      data.remove(0, data.indexOf(char(0x24)));
       data.remove(data.lastIndexOf(char(0x2A)) + 3);
       data.trim();
       return data;
     }
+
+    void sendCommand(String command) {
+      _serial->println(command);
+    }
+
 };
 
 #endif
