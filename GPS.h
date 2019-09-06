ce8760d5cbb9771fb849b810aea90adfd6651fc4
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-06 20:30:02 +0100

    Update GPS.h

--- GPS.h
+++ GPS.h
@@ -4,34 +4,37 @@
 #include <SoftwareSerial.h>
 
 class GPS {
-  private:
-    SoftwareSerial *_serial;
-    uint16_t _length;
-
   public:
-    explicit GPS(uint8_t rx, uint16_t length = 800): _length(length) {
-      _serial = new SoftwareSerial(rx, rx);
+    SoftwareSerial *_serial;
+
+    GPS(uint8_t rx, uint8_t tx) {
+      _serial = new SoftwareSerial(rx, tx);
     }
 
-    void begin(uint32_t baud) {_serial->begin(baud);
-      _serial->begin(9600);
+    void begin(uint32_t baud) {
+      _serial->begin(baud);
     }
 
-    String readString() {
+    uint8_t checksum(String data) {
+      uint8_t n = 0;
+      for (uint8_t i = 0; i < data.length(); i++) n ^= byte(data[i]);
+      return n;
+    }
+
+    String readString(uint16_t length = 800) {
       String data = String(0x24);
-      uint16_t lenght = _length;
       while (!_serial->available() || (_serial->read() != 0x24));
-      do if (_serial->available() && (data += char(_serial->read())) && lenght--); while (lenght);
+      do if (_serial->available() && (data += char(_serial->read())) && length--); while (length);
       data.remove(0, data.indexOf(char(0x24)));
       data.remove(data.lastIndexOf(char(0x2A)) + 3);
       data.trim();
       return data;
     }
 
-    void sendCommand(String command) {
-      _serial->println(command);
+     String write(String data) {
+      if (_serial->print(char(0x24) + data + char(0x2A) + String(checksum(data), HEX) + char(0x0D) + char(0x0A))) return readString();
+      return "";
     }
-
 };
 
 #endif
