b1ef44d97d7cf9e58fa2690e125bf3035c3dd1bc
offcircuit <35690030+offcircuit@users.noreply.github.com>
2019-10-02 23:15:37 +0100

    Add files via upload

--- GPS.cpp
+++ GPS.cpp
@@ -34,9 +34,9 @@ String GPS::find(char *buffer) {
         data += char(c);
       } while (c != 0x2A);
     while (_serial->available() < 2);
+    c = data.length();
     data += char(_serial->read());
     data += char(_serial->read());
-    c = data.lastIndexOf(char(0x2A)) + 1;
     if (prefix(n, HEX).equalsIgnoreCase(data.substring(c, c + 2))) return data;
   }
   return "";
